7bc41c70b02322cc1e9ebbccf32f54846fa4501c
Junaid Qureshi <Junaid.562000@gmail.com>
2025-01-05 14:33:32 +0530

    serverless environment config

--- db/seed.ts
+++ db/seed.ts
@@ -1,9 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 
 import sampleData from "./sample-data";
 
 async function main() {
-  const prisma = new PrismaClient();
   await prisma.product.deleteMany();
   await prisma.product.createMany({ data: sampleData.products });
   console.log("database seeded successfully");
